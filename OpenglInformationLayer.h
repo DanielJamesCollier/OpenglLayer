6e44d3766de94ee6ef6357c7b8a85086bd596be5
Daniel Collier <daniel.collier@hotmail.co.uk>
2016-12-10 22:14:11 +0000

    edit formating

--- OpenglInformationLayer.h
+++ OpenglInformationLayer.h
@@ -37,11 +37,8 @@ class OpenglInformationLayer {
 public:
     
     void init() {
-        /* context information */
+        /* Context information */
         //------------------------------------------------------------------------------------------------------//
-        
-        // check if core profile and if forward compatible
-        //----------/
         GLint mask;
         glGetIntegerv(GL_CONTEXT_PROFILE_MASK, &mask);
         if((mask & GL_CONTEXT_CORE_PROFILE_BIT) == 0) {
@@ -50,7 +47,7 @@ public:
         } else {
             m_isCoreProfile = true;
         }
-   
+        
         GLint flags;
         glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
         if((flags & GL_CONTEXT_FLAG_FORWARD_COMPATIBLE_BIT) != 0) {
@@ -58,32 +55,26 @@ public:
         } else {
             m_isProfileForwardCompatible = false;
         }
-        //----------/
-        
         
         // Check Vendor
         /*
-         It could be "ATI Technologies", "NVIDIA Corporation", "INTEL" and so on. Note that there's no guarantee that the string for a specific vendor will remain the same in future implementations. On Windows, if it says "Microsoft" then you are using the Windows software renderer or the Windows Direct3D wrapper. You probably haven't installed the graphics drivers yet in that case.
-        */
-        //----------/
+         It could be "ATI Technologies", "NVIDIA Corporation", "INTEL" and so on. Note that there's no guarantee that the string for a specific vendor will remain the same in future
+         implementations. On Windows, if it says "Microsoft" then you are using the Windows software renderer or the Windows Direct3D wrapper. You probably haven't installed the graphics
+         drivers yet in that case.
+         */
         m_vendor = (const char *)glGetString(GL_VENDOR);
-        //----------/
         
         //check renderer
         /*
          This string is often the name of the GPU. In the case of Mesa3d, it would be i.e "Gallium 0.4 on NVA8".
          It might even say "Direct3D" if the Windows Direct3D wrapper is being used.
-        */
-        //----------/
+         */
         m_renderer = (const char *)glGetString(GL_RENDERER);
-        //----------/
-    
+        
         // get context major and minor version
-        //----------/
         GL_CHECK(glGetIntegerv(GL_MAJOR_VERSION, &m_majorVersion)); // function works for opengl 3.0 + only
         GL_CHECK(glGetIntegerv(GL_MINOR_VERSION, &m_minorVersion)); // function works for opengl 3.0 + only
-        //----------/
-        //------------------------------------------------------------------------------------------------------//
+        
         
         /* texture information */
         //------------------------------------------------------------------------------------------------------//
@@ -91,9 +82,10 @@ public:
         GL_CHECK(glGetIntegerv(GL_MAX_TEXTURE_IMAGE_UNITS,            &m_maxTextureImageUnits));
         GL_CHECK(glGetFloatv  (GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT,     &m_maxAnisotropy));
         GL_CHECK(glGetIntegerv(GL_MAX_TEXTURE_SIZE,                   &m_maxTextureSize));
+        
+        
+        // build information string
         //------------------------------------------------------------------------------------------------------//
-        
-        
         m_concatedInfo += "--------------------------------------\n";
         m_concatedInfo += "      OpenGL Information Layer        \n";
         m_concatedInfo += "--------------------------------------\n";
@@ -104,7 +96,7 @@ public:
         m_concatedInfo += "Vendor:                           " + std::string(m_vendor) + '\n';
         m_concatedInfo += "Renderer:                         " + std::string(m_renderer) + "\n";
         m_concatedInfo += "------------------                    \n\n";
-
+        
         
         m_concatedInfo += "------------------                    \n";
         m_concatedInfo += "Texture Info\n";
